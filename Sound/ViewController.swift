0869f0dd5dfcfada70cd9b22783e2bae0337c6db
Futaba3 <nyokinyoki41aya@gmail.com>
2022-05-08 16:17:05 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by 工藤彩名 on 2022/04/29.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var drumButton: UIButton!
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
+    
+    let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound")!.data)
+    let pianoSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    let guitarSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // ドラムのタップが始まった時に呼ばれる
+    @IBAction func touchDownDrumButton(){
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for: .normal)
+        
+        drumSoundPlayer.currentTime = 0
+        
+        drumSoundPlayer.play()
+    }
+    
+    // ドラムのタップが終わった時に呼ばれる
+    @IBAction func touchUpDrumButton() {
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+        
+    }
+    
+    @IBAction func touchDownPianoButton() {
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"), for: .normal)
+        
+        pianoSoundPlayer.currentTime = 0
+        
+        pianoSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpPianoButton() {
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownGuitarButton() {
+        guitarButton.setImage(UIImage(named: "guitarPlayingImage"), for: .normal)
+        
+        guitarSoundPlayer.currentTime = 0
+        
+        guitarSoundPlayer.play()
+    }
+    
+    @IBAction func touchUpGuitarButton() {
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
+    }
+
+
+
+}
+
